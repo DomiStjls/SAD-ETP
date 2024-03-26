c3323a8a07e8f9554a06aedc1c3a2cb9f9622819
MaksimKharlamov <MaksimKharlamov35@yandex.ru>
2024-03-26 18:28:05 +0300

    работает вход в аккаунт

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 from flask import *
+from flask_login import *
 from data import db_session
-from data.item import Item
+from data.loginform import LoginForm
+from data.user import User
 
 DEBUG = True
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandex_lyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route("/")
