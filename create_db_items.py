0fcd39bd5c3e3fe79ca7d09fec90ea831af6d176
Darya Shevchenko <123009141+DomiStjls@users.noreply.github.com>
2024-03-30 21:54:08 +0300

    заполнение бд вещей

--- create_db_items.py
+++ create_db_items.py
@@ -1,1 +1,30 @@
 # тут будет код который загрузит информацию из prices.xlsx в базу данных
+
+import os
+
+from data import db_session
+from data.user import User
+from data.item import Item
+
+try:
+    # os.system('type nul > db/shop.db')
+
+    db_session.global_init("db/shop.db")
+    db_sess = db_session.create_session()
+    with open("prices.csv", encoding='utf-8') as f:
+        r = f.readlines()[1:]
+        for i in range(16):
+            s = r[i].split(";")
+            category = s[0]
+            name = s[5]
+            description = s[9]
+            photo = s[12]
+            price = int(s[7].split(',')[0])
+            item = Item(name=name,category=category,description=description,photo=photo,price=price)
+            db_sess.add(item)
+
+
+    db_sess.commit()
+
+except Exception as e:
+    print(e)
